167174849c20ab963057363ff5fb208f5f24876d
Donavan Grobler <donavangrobler2@gmail.com>
2021-12-08 16:53:34 +0200

    Navigation bars for portfolio

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,23 @@
+<nav> 
+    <ul>
+        <li> 
+            <a href="/">Homepage</a>
+        </li>
+        <li> 
+            <a href="/about">About</a>
+        </li>
+        <li> 
+            <a href="/contact">Contact</a>
+        </li>
+    </ul>
+</nav>
+
+<style>
+    ul {
+        display: flex;
+        list-style: none;
+    }
+    li {
+        margin: 10px;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,2 @@
 <h1>Welcome to Donavan's Website</h1>
-<p>Contract me for amazing development skills </p>
-<p>Test Paragraph </p>
+<p>Contract me for amazing development skills</p>
