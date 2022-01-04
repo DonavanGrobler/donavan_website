637c41b05c4d579c1c2227293e31bafac8b8fa93
Donavan Grobler <donavangrobler2@gmail.com>
2022-01-04 11:38:03 +0200

    _ BlogPosts

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,2 +1,48 @@
+<script context="module">
+    const allPosts = import.meta.glob('./*.md');
+
+    let blog = [];
+
+    for (let path in allPosts) {
+        blog.push (
+            allPosts[path]().then(({metadata}) => { 
+                return {path, metadata};
+            })
+        );
+    }
+
+    console.log('blog=', blog);
+
+    export const load = async() => {
+        const posts = await Promise.all(blog);
+        return {
+            props: {
+                posts,
+            }
+        }
+    };
+</script>
+
+<script>
+    export let posts;
+    console.log('posts=', posts);
+</script>
+
+<div>
+    {#each posts as {path, metadata}}
+        <article>
+            <h2>
+                {metadata.title}
+            </h2>
+            <p>
+                {metadata.description}
+            </p>
+            <a href="{path}">Read More</a>
+        
+        </article>
+    {/each}
+</div>
+
 <h1>Donavan's Blog Page</h1>
+
 
